f13fd312a04df8df46ef8f0b6ce5ae06f46423e9
sriley <sarahriley.dev@gmail.com>
2019-06-14 16:08:00 -0400

    server running

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
 
-const router = require('./router');
 
 
-app.use(logger("dev"));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('*', router);
+
 
 
 app.get("/", (req, res) => {
@@ -16,6 +15,62 @@ app.get("/", (req, res) => {
   );
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const UssdMenu = require('ussd-menu-builder');
+let menu = new UssdMenu();
+ 
+// Define menu states
+menu.startState({
+    run: () => {
+        // use menu.con() to send response without terminating session      
+        menu.con('Welcome. Choose option:' +
+            '\n1. Show Balance' +
+            '\n2. Buy Airtime');
+    },
+    // next object links to next state based on user input
+    next: {
+        '1': 'showBalance',
+        '2': 'buyAirtime'
+    }
 });
+ 
+menu.state('showBalance', {
+    run: () => {
+        // fetch balance
+        fetchBalance(menu.args.phoneNumber).then(function(bal){
+            // use menu.end() to send response and terminate session
+            menu.end('Your balance is KES ' + bal);
+        });
+    }
+});
+ 
+menu.state('buyAirtime', {
+    run: () => {
+        menu.con('Enter amount:');
+    },
+    next: {
+        // using regex to match user input to next state
+        '*\\d+': 'buyAirtime.amount'
+    }
+});
+ 
+// nesting states
+menu.state('buyAirtime.amount', {
+    run: () => {
+        // use menu.val to access user input value
+        var amount = Number(menu.val);
+        buyAirtime(menu.args.phoneNumber, amount).then(function(res){
+            menu.end('Airtime bought successfully.');
+        });
+    }
+});
+ 
+// Registering USSD handler with Express
+ 
+app.post('/ussd', function(req, res){
+    menu.run(req.body, ussdResult => {
+        res.send(ussdResult);
+    });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
