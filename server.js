a31a38ed2d8b52b275443dbd682a6a08133da1bf
sriley <sarahriley.dev@gmail.com>
2019-06-17 16:19:47 -0400

    test post

--- server.js
+++ server.js
@@ -1,76 +1,96 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
 
-
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
 app.get("/", (req, res) => {
-  res.send(
-    "It's alive!"
-  );
+  res.send("It's alive!");
 });
 
-const UssdMenu = require('ussd-menu-builder');
+const UssdMenu = require("ussd-menu-builder");
 let menu = new UssdMenu();
- 
+
 // Define menu states
 menu.startState({
-    run: () => {
-        // use menu.con() to send response without terminating session      
-        menu.con('Welcome. Choose option:' +
-            '\n1. Show Balance' +
-            '\n2. Buy Airtime');
-    },
-    // next object links to next state based on user input
-    next: {
-        '1': 'showBalance',
-        '2': 'buyAirtime'
-    }
+  run: () => {
+    // use menu.con() to send response without terminating session
+    menu.con("Welcome. Choose option:" + "\n1. Buyer" + "\n2. Seller");
+  },
+  // next object links to next state based on user input
+  next: {
+    "1": "markets",
+    "2": "postForSale"
+  }
 });
- 
-menu.state('showBalance', {
-    run: () => {
-        // fetch balance
-        fetchBalance(menu.args.phoneNumber).then(function(bal){
-            // use menu.end() to send response and terminate session
-            menu.end('Your balance is KES ' + bal);
-        });
-    }
+
+menu.state("markets", {
+  run: () => {
+    menu.con(
+      "Welcome. Choose option:" +
+        "\n1. Bujumbaru" +
+        "\n2. Tororo" +
+        "\n3. Mbale" +
+        "\n4. Eldoret" +
+        "\n5. Kisumu" +
+        "\n6. Soroti" +
+        "\n7. Ownio" +
+        "\n8. Kampala"
+    );
+  },
+  next: {
+    "1": "Bujumbaru",
+    "2": "Tororo",
+    "3": "Mbale",
+    "4": "Eldoret",
+    "5": "Kisumu",
+    "6": "Soroti",
+    "7": "Ownio",
+    "8": "Kampala"
+  }
 });
- 
-menu.state('buyAirtime', {
-    run: () => {
-        menu.con('Enter amount:');
-    },
-    next: {
-        // using regex to match user input to next state
-        '*\\d+': 'buyAirtime.amount',
-        '99': 'home'
-    }
-});
- 
-// nesting states
-menu.state('buyAirtime.amount', {
-    run: () => {
-        // use menu.val to access user input value
-        var amount = Number(menu.val);
-        buyAirtime(menu.args.phoneNumber, amount).then(function(res){
-            menu.end('Airtime bought successfully.');
-        });
-    }
-});
- 
-// Registering USSD handler with Express
- 
-app.post('*', function(req, res){
-    menu.run(req.body, ussdResult => {
-        res.send(ussdResult);
+
+menu.state("Bujumbaru", {
+  run: () => {
+    // fetch balance
+    fetchBalance(menu.args.phoneNumber).then(function(bal) {
+      // use menu.end() to send response and terminate session
+      menu.end("Your balance is KES " + bal);
     });
+  }
+});
+
+menu.state("postForSale", {
+  run: () => {
+    async function addPost(post) {
+      console.log("before");
+      const func = await db("markets")
+        .insert(post)
+        .where({ market: market });
+      console.log("after");
+      return `New Post ID: ${post.name} : Added :)`;
+    }
+    menu.end("Your post is live" + post);
+  }
+});
+
+// nesting states
+menu.state("buyAirtime.amount", {
+  run: () => {
+    // use menu.val to access user input value
+    var amount = Number(menu.val);
+    buyAirtime(menu.args.phoneNumber, amount).then(function(res) {
+      menu.end("Airtime bought successfully.");
+    });
+  }
+});
+
+// Registering USSD handler with Express
+
+app.post("*", function(req, res) {
+  menu.run(req.body, ussdResult => {
+    res.send(ussdResult);
+  });
 });
 
 const port = process.env.PORT || 5000;
