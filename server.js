718b5e4f47981b6828aa34e731a257a1ea5f09d4
sriley <sarahriley.dev@gmail.com>
2019-06-19 16:34:08 -0400

    test

--- server.js
+++ server.js
@@ -2,24 +2,22 @@ const app = require("express")();
 const bodyParser = require("body-parser");
 const db = require("./data/dbConfig");
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
 app.get("/products", (req, res) => {
-    db("products")
+  db("products")
     .then(products => {
-        res.status(200).json(products);
-      })
-      .catch(err => {
-        res.status(400).json({message: "Fail"})
-      });
-})
+      res.status(200).json(products);
+    })
+    .catch(err => {
+      res.status(400).json({ message: "Fail" });
+    });
+});
 
 const UssdMenu = require("ussd-menu-builder");
 let menu = new UssdMenu();
@@ -39,37 +37,18 @@ menu.startState({
 
 menu.state("markets", {
   run: () => {
-    menu.con(
-      "Welcome. Choose option:" +
-        "\n1. Bujumbaru" +
-        "\n2. Tororo" +
-        "\n3. Mbale" +
-        "\n4. Eldoret" +
-        "\n5. Kisumu" +
-        "\n6. Soroti" +
-        "\n7. Ownio" +
-        "\n8. Kampala"
-    );
+    const markets = products.market.filter(item => item.menu.val);
+    menu.con("Welcome. Choose option:");
+    return markets;
   },
   next: {
-    "1": "Bujumbaru",
-    "2": "Tororo",
-    "3": "Mbale",
-    "4": "Eldoret",
-    "5": "Kisumu",
-    "6": "Soroti",
-    "7": "Ownio",
-    "8": "Kampala"
+    "*\\d+": "Test"
   }
 });
 
-menu.state("Bujumbaru", {
+menu.state("Test", {
   run: () => {
-    // fetch balance
-    fetchBalance(menu.args.phoneNumber).then(function(bal) {
-      // use menu.end() to send response and terminate session
-      menu.end("Your balance is KES " + bal);
-    });
+    menu.end("You made it!");
   }
 });
 
@@ -103,8 +82,6 @@ menu.state("addCountry", {
       });
   }
 });
-
-
 
 // Registering USSD handler with Express
 
