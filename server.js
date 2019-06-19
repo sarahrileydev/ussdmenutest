05b6eea16c9217148d6c05b2535e0c3ad24c9102
sriley <sarahriley.dev@gmail.com>
2019-06-19 16:11:55 -0400

    retest

--- server.js
+++ server.js
@@ -73,6 +73,37 @@ menu.state("Bujumbaru", {
   }
 });
 
+menu.state("postForSale", {
+  run: () => {
+    menu.con("Enter a country:");
+  },
+  next: {
+    "*[a-zA-Z]+": "addCountry"
+  }
+});
+
+// nesting states
+menu.state("addCountry", {
+  run: () => {
+    // use menu.val to access user input value
+    let country = menu.val;
+    const product = {
+      country: country,
+      market: "market",
+      product: "product",
+      price: "price"
+    };
+    db("products")
+      .insert(product)
+      .then(res => {
+        menu.end("Country added successfully!");
+      })
+      .catch(err => {
+        menu.end("Fail");
+      });
+  }
+});
+
 
 
 // Registering USSD handler with Express
