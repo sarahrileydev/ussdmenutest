3810e68d1fdf4c98af67ba9b7f354f23487509fd
sriley <sarahriley.dev@gmail.com>
2019-06-19 13:36:29 -0400

    breakout seller menu

---
+++ sellerStates.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+
+const UssdMenu = require('ussd-menu-builder')
+
+
+const menu = new UssdMenu()
+
+
+const bodyParser = require('body-parser')
+
+
+menu.state("postForSale", {
+  run: () => {
+    menu.con("Enter a country:");
+  },
+  next: {
+    "*[a-zA-Z]+": "addCountry"
+  }
+});
+
+// nesting states
+menu.state("addCountry", {
+  run: () => {
+    // use menu.val to access user input value
+    let country = menu.val;
+    const product = {
+      country: country,
+      market: "market",
+      product: "product",
+      price: "price"
+    };
+    db("products")
+      .insert(product)
+      .then(res => {
+        menu.end("Country added successfully!");
+      })
+      .catch(err => {
+        menu.end("Fail");
+      });
+  }
+});
+
+
+module.exports = sellerStates;
