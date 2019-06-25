240075f00bcecefde96745b6e193ad8e9e1af630
sriley <sarahriley.dev@gmail.com>
2019-06-25 17:51:14 -0400

    test

--- server.js
+++ server.js
@@ -22,6 +22,60 @@ app.get("/products", (req, res) => {
 const UssdMenu = require("ussd-menu-builder");
 let menu = new UssdMenu();
 
+function findPrice(name, product) {
+  return db("markets")
+    .select("price")
+    .where({ name: name, product: product });
+}
+
+async function addProduct(product) {
+  const [id] = await db('products').insert(product)
+
+}
+
+function get() {
+  return db('marketplaces')
+}
+
+function getMarkets() {
+  return db('marketplaces')
+}
+
+function getCat() {
+  return db('categories')
+}
+
+async function categories(){
+  const result = await models.getCat()
+  return result
+}
+
+async function products(){
+  const result = await models.getProducts()
+  return result
+}
+
+async function getProducts() {
+  const products = await db('products')
+  // add a temporary "seller_id" -- we need to add this to the actual database
+  return products.map(product => {
+    product.seller_id = product.id
+    return product
+  })
+}
+
+function getSellers () {
+  const sellers = []
+  for (let i=0; i<100; i++) {
+    sellers.push({
+      id: i,
+      name: `This Guy ${i}`,
+      phone: '555-555-${i}${i}${i}${i}'
+    })
+  }
+  return sellers
+}
+
 // Menu & State Generators
 // ------------------------------------------------------------------------------------------
 // */
